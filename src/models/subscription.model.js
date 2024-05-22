5d593f341bfd1ee3dfe843fcc7fa18d605599797
@anshukr <116056170+AnshuKumar7673@users.noreply.github.com>
2024-05-22 13:27:51 +0530

    Update subscription.model.js

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,20 +1,16 @@
-import mongoose,{Schema} from "mongoose"
+import mongoose, {Schema} from "mongoose"
 
-const scubscriptionSchema=new Schema({
-    Subsciption:{
-      
-        type: Schema.Types.ObjectId, //subscribing jo karraga
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribing
         ref: "User"
     },
-    channel:{
-        type: Schema.Types.ObjectId,
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
         ref: "User"
     }
-},
-{
-    timestamps:true
-})
+}, {timestamps: true})
 
 
- 
-export const Subsciption=mongoose.model("Subsciption",scubscriptionSchema)
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
