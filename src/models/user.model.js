8eb8d436707f5ba7b3a139e4a8dc51901bce9f8b
@anshukr <116056170+AnshuKumar7673@users.noreply.github.com>
2024-05-22 13:29:48 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,106 +1,93 @@
-import mongoose,{Schema} from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-
-const useSchema=new Schema(
+const userSchema = new Schema(
     {
-        username:{
+        username: {
             type: String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true,
-            index:true
-
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
         },
-        email:{               
-                    type: String,
-                    required:true,
-                    unique:true,
-                    lowercase:true,
-                    trim:true,
-                   
-        },
-        fullName:{
-            
+        email: {
             type: String,
-            required:true,
-             
-            
-            trim:true,
-            index:true
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
         },
-        avatar:{           
-            type: String,//cloudinary url
-            required:true
-             
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
         },
-        coverImage:{
-            type:String
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
         },
-        watchHistory:[
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
             {
-                type:Schema.Types.ObjectId,
-                ref:"Video"
+                type: Schema.Types.ObjectId,
+                ref: "Video"
             }
         ],
-        password:{
-            type:String,
-            require:[true,'password is requred']
+        password: {
+            type: String,
+            required: [true, 'Password is required']
         },
-        refreshToken:{
-            type:String
-        },
-       
-},
-   {
-      timestamps:true
-   }
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
 )
 
-useSchema.pre("save",async function (next){
-    if(!this.isModified("password"))  return next();
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
 
-    this.password=await bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
-useSchema.method.isPasswordCorrect =async function (password){
-  return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
 }
 
-useSchema.method.generateAccessToken=function(){
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
-            _id:this._id,
-            email:this.email,
-            username:this.username,
-            fullName:this.fullName,
-
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-           expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
-        
-
     )
 }
-useSchema.method.generateRefreshToken=function(){
-
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
-            _id:this._id,
-             
+            _id: this._id,
+            
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-           expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
-        
-
     )
 }
 
-export const User =mongoose.model("User",useSchema)
+export const User = mongoose.model("User", userSchema)
