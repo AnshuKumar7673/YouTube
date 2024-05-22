2ac415c108deda47492188b172caa74397ac35b3
@anshukr <116056170+AnshuKumar7673@users.noreply.github.com>
2024-05-22 13:23:27 +0530

    Update multere.middleware.js

--- src/middlewares/multere.middleware.js
+++ src/middlewares/multere.middleware.js
@@ -1,20 +1,15 @@
 import multer from "multer";
-const multerMiddleware = require('../middlewares/multer.middleware');
-
-
-
-// he disk storage engine gives you full control on storing files to disk.
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null,"./public/temp" )
-  },
-  filename: function (req, file, cb) {
-    
-    cb(null, file.originalname)
-  }
-})
-
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)
+    }
+  })
+  
 export const upload = multer({ 
-    storage,
- })
+    storage, 
+})
